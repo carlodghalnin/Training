50136817d63888ed73185335b61619cb07db31e5
Carlo de Guzman Halnin <87485233+carlodghalnin@users.noreply.github.com>
2021-12-13 13:40:47 -0800

    Add files via upload

--- SQL/MS SQL Server/Advanced SQL Practice Problems.sql
+++ SQL/MS SQL Server/Advanced SQL Practice Problems.sql
@@ -124,13 +124,101 @@ SELECT EmployeeID, COUNT(EmployeeID) AS NumberLates
 
 /*43. The results in #42 need to be compared against 
 the total number of orders per salesperson. */
-WITH LateOrders (EmployeeID, OrderID, 
-	RequiredDate, ShippedDate) AS 
-	(SELECT EmployeeID, OrderID, RequiredDate, ShippedDate
-	FROM Orders WHERE RequiredDate <= ShippedDate),
-	Ordered (EmployeeOrder) AS 
-	(SELECT OrderID FROM Orders)
-SELECT EmployeeID, COUNT(EmployeeID) AS NumberLates,
-	COUNT(EmployeeOrder) AS NumberTotal
-	FROM LateOrders, Ordered GROUP BY EmployeeID
-	ORDER BY NumberLates DESC;
+WITH Lates (EmployeeIDL, LateOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders 
+	WHERE RequiredDate <= ShippedDate 
+	GROUP BY EmployeeID),
+	Totals (EmployeeIDT, TotalOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders  
+	GROUP BY EmployeeID)
+SELECT L.EmployeeIDL, E.LastName, L.LateOrders,
+	T.TotalOrders FROM Lates L JOIN Totals T
+	ON L.EmployeeIDL = T.EmployeeIDT
+	LEFT JOIN Employees E
+	ON L.EmployeeIDL = E.EmployeeID
+	ORDER BY L.EmployeeIDL;
+
+--44. Include the missing employee in #43.
+WITH Lates (EmployeeIDL, LateOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders 
+	WHERE RequiredDate <= ShippedDate 
+	GROUP BY EmployeeID),
+	Totals (EmployeeIDT, TotalOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders  
+	GROUP BY EmployeeID)
+SELECT T.EmployeeIDT, E.LastName, L.LateOrders,
+	T.TotalOrders 
+	FROM Totals T LEFT JOIN Lates L 
+	ON T.EmployeeIDT = L.EmployeeIDL
+	LEFT JOIN Employees E
+	ON T.EmployeeIDT = E.EmployeeID
+	ORDER BY T.EmployeeIDT;
+
+--45. Replace #44's NULL with 0.
+WITH Lates (EmployeeIDL, LateOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders 
+	WHERE RequiredDate <= ShippedDate 
+	GROUP BY EmployeeID),
+	Totals (EmployeeIDT, TotalOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders  
+	GROUP BY EmployeeID)
+SELECT T.EmployeeIDT, E.LastName, 
+	ISNULL(L.LateOrders, 0) AS LateOrders,
+	T.TotalOrders 
+	FROM Totals T LEFT JOIN Lates L 
+	ON T.EmployeeIDT = L.EmployeeIDL
+	LEFT JOIN Employees E
+	ON T.EmployeeIDT = E.EmployeeID
+	ORDER BY T.EmployeeIDT;
+
+--46. Get percentage of late versus total orders.
+WITH Lates (EmployeeIDL, LateOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders 
+	WHERE RequiredDate <= ShippedDate 
+	GROUP BY EmployeeID),
+	Totals (EmployeeIDT, TotalOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders  
+	GROUP BY EmployeeID)
+SELECT T.EmployeeIDT, E.LastName, 
+	ISNULL(L.LateOrders, 0) AS LateOrders,
+	T.TotalOrders, 
+	ISNULL((LateOrders/CAST(T.TotalOrders 
+	AS DECIMAL)), 0) AS PercentLates
+	FROM Totals T LEFT JOIN Lates L 
+	ON T.EmployeeIDT = L.EmployeeIDL
+	LEFT JOIN Employees E
+	ON T.EmployeeIDT = E.EmployeeID
+	ORDER BY T.EmployeeIDT;
+
+--46. Cut off decimals in #45 to 2 places.
+WITH Lates (EmployeeIDL, LateOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders 
+	WHERE RequiredDate <= ShippedDate 
+	GROUP BY EmployeeID),
+	Totals (EmployeeIDT, TotalOrders) AS 
+	(SELECT EmployeeID, COUNT(*) FROM Orders  
+	GROUP BY EmployeeID)
+SELECT T.EmployeeIDT, E.LastName, 
+	ISNULL(L.LateOrders, 0) AS LateOrders,
+	T.TotalOrders, 
+	CONVERT(DECIMAL(10,2), 
+	ISNULL((LateOrders/CONVERT(DECIMAL(5,2), 
+	T.TotalOrders)), 0)) 
+	AS PercentLates
+	FROM Totals T LEFT JOIN Lates L 
+	ON T.EmployeeIDT = L.EmployeeIDL
+	LEFT JOIN Employees E
+	ON T.EmployeeIDT = E.EmployeeID
+	ORDER BY T.EmployeeIDT;
+
+/*48. Group customers by 2016 orders: 0 to 1,000,
+1,000 to 5,000, 5,000 to 10,000, and over 10,000.
+Order by customer ID.*/
+SELECT C.CustomerID, C.CompanyName,
+	SUM(D.UnitPrice * D.Quantity) AS TotalOrders 
+	FROM Customers C
+	JOIN Orders O ON C.CustomerID = O.CustomerID
+	JOIN OrderDetails D ON O.OrderID = D.OrderID
+	WHERE CAST(O.OrderDate AS DATE) LIKE '2016%'
+	GROUP BY C.CustomerID, C.CompanyName
+	ORDER BY C.CustomerID;
